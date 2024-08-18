9eca5a8d884d0cb39b8283209ab4281c2886876b
Ahmed Mohamed <aahmedalmubarak@gmail.com>
2024-08-18 08:38:21 +0200

    Add folders strucure

---
+++ URLShortener.API/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+app.MapControllers();
+
+
+
+app.Run();
+
