cc96a029728e3254b7ac420c81c6ff14a04f7d10
Ahmed Mohamed <aahmedalmubarak@gmail.com>
2024-08-20 07:57:55 +0200

    refactor some code in contrller

--- URLShortener.API/Controllers/V1/TinyLinkController.cs
+++ URLShortener.API/Controllers/V1/TinyLinkController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using TinyLink.API.Models;
 using TinyLink.API.Services;
+using TinyLink.API.Utilities;
 
 namespace TinyLink.API.Controllers.V1
 {
@@ -26,7 +27,7 @@ namespace TinyLink.API.Controllers.V1
         [Route("QueryTinyLink")]
         public async Task<IActionResult> QueryTinyLink(TinyLinkRequest tinyLinkRequest)
         {
-            var ipAddress = GetClientIpAddress(HttpContext);
+            var ipAddress = GetIpAddressHelper.GetClientIpAddress(HttpContext);
             var userAgent = HttpContext.Request.Headers["User-Agent"];
 
             var query = new TinyLinkQuery
@@ -40,15 +41,6 @@ namespace TinyLink.API.Controllers.V1
             var originalTinyLink = await _tinyLinkService.GetOriginalLink(query);
             return Redirect(originalTinyLink);
         }
-        private string GetClientIpAddress(HttpContext httpContext)
-        {
-            var clientIp = httpContext.Request.Headers["X-Real-IP"].ToString();
 
-            if (string.IsNullOrEmpty(clientIp))
-            {
-                clientIp = httpContext.Connection.RemoteIpAddress.ToString();
-            }
-            return clientIp;
-        }
     }
 }
---
+++ URLShortener.API/Utilities/GetIpAddressHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace TinyLink.API.Utilities
+{
+    public class GetIpAddressHelper
+    {
+        public static string GetClientIpAddress(HttpContext httpContext)
+        {
+            var clientIp = httpContext.Request.Headers["X-Real-IP"].ToString();
+
+            if (string.IsNullOrEmpty(clientIp))
+            {
+                clientIp = httpContext.Connection.RemoteIpAddress.ToString();
+            }
+            return clientIp;
+        }
+    }
+}
